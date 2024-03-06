e43a0e8b50061d637f1cbab587a74f354ea8c746
temitope fadipe <temitopefadipe@yahoo.com>
2024-03-06 12:25:54 +0100

    mongodb connection

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 require("dotenv").config()
 const express = require("express")
-
+const connectDB = require("./config/dbConfig")
+const mongoose = require("mongoose")
 // initialize express object
 const app = express()
 
 // for Json Data
 app.use(express.json())
-
+connectDB()
 // Port
 const PORT = process.env.PORT || 5890
 
 // connect to server
-app.listen(PORT, () => {
-    console.log(`server is running on port http://localhost:${PORT}`)
+mongoose.connection.once("open", () => {
+    console.log("Database connection successful")
+    
+    app.listen(PORT, () => {
+        console.log(`server is running on port http://localhost:${PORT}`)
+    })
 })
